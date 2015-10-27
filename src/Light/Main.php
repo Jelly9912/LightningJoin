cc1472278f03588f781cd6449b9d0aaa126a64f6
Jelly9912 <taytan9912@gmail.com>
2015-10-27 16:26:03 +0100

    Create Main.php

---
+++ src/Light/Main.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Light;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\network\protocol\SetTimePacket;
+use pocketmine\network\protocol\TextPacket;
+use pocketmine\network\protocol\AddPlayerPacket;
+use pocketmine\entity\Entity;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(){
+    $this->getServer()->getLogger()->info("LightingJoin enabled!");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+    }
+   
+    public function onJoin(PlayerJoinEvent $e)
+		{
+			$p = $e->getPlayer();
+		$light = new AddEntityPacket();
+        $light->type = 93;
+        $light->eid = Entity::$entityCount++;
+        $light->metadata = array();
+        $light->speedX = 0;
+        $light->speedY = 0;
+        $light->speedZ = 0;
+        $light->yaw = $p->getYaw();
+        $light->pitch = $p->getPitch();
+        $light->x = $p->x;
+        $light->y = $p->y;
+        $light->z = $p->z;
+        $p->dataPacket($light);
+		}
+	}
